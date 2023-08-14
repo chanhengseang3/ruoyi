9a88090571662cb17f5e107c1c6b5b974a188b90
chanheng <seang.chanheng@mbanq.com>
2023-08-14 17:08:49 -0600

    add 81

---
+++ ruoyi-admin/src/main/java/com/ruoyi/EmbeddedTomcatConfiguration.java
@@ -0,0 +1,36 @@
+package com.ruoyi;
+
+import org.apache.catalina.connector.Connector;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.embedded.tomcat.ConfigurableTomcatWebServerFactory;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class EmbeddedTomcatConfiguration {
+
+    @Value("${server.additionalPort}")
+    private String additionalPort;
+
+    @Bean
+    public ConfigurableTomcatWebServerFactory servletContainer() {
+        var tomcat = new TomcatServletWebServerFactory();
+        var additionalConnector = this.additionalConnector();
+        if (additionalConnector != null) {
+            tomcat.addAdditionalTomcatConnectors(additionalConnector);
+        }
+        return tomcat;
+    }
+
+    private Connector additionalConnector() {
+        if (StringUtils.isBlank(this.additionalPort)) {
+            return null;
+        }
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setScheme("http");
+        connector.setPort(Integer.parseInt(additionalPort));
+        return connector;
+    }
+}
