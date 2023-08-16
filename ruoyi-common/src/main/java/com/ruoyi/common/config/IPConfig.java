3acdddbec358960d0584ced9c858ad5a65a49ad6
chanheng <seang.chanheng@mbanq.com>
2023-08-16 12:01:12 -0600

    disable log

--- ruoyi-common/src/main/java/com/ruoyi/common/config/IPConfig.java
+++ ruoyi-common/src/main/java/com/ruoyi/common/config/IPConfig.java
@@ -5,6 +5,7 @@ import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.util.StopWatch;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.*;
@@ -20,40 +21,40 @@ public class IPConfig {
     private static final Logger log = LoggerFactory.getLogger(IPConfig.class);
 
     public static String getIpAddr(HttpServletRequest request) {
-        String agent= request.getHeader("user-agent");
+        String agent = request.getHeader("user-agent");
         return agent;
     }
 
-    public static String getIp(HttpServletRequest request){
+    public static String getIp(HttpServletRequest request) {
         String ipAddress = null;
 
         ipAddress = request.getHeader("x-forwarded-for");
-        if(ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
+        if (ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
             ipAddress = request.getHeader("Proxy-Client-IP");
         }
-        if(ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
+        if (ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
             ipAddress = request.getHeader("WL-Proxy-Client-IP");
         }
-        if(ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
+        if (ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
             ipAddress = request.getRemoteAddr();
-            if(ipAddress.equals("127.0.0.1")){
+            if (ipAddress.equals("127.0.0.1")) {
                 //根据网卡取本机配置的IP
-                InetAddress inet=null;
+                InetAddress inet = null;
                 try {
                     inet = InetAddress.getLocalHost();
                 } catch (UnknownHostException e) {
                     e.printStackTrace();
                 }
-                ipAddress= inet.getHostAddress();
+                ipAddress = inet.getHostAddress();
             }
 
         }
 
 
         //对于通过多个代理的情况，第一个IP为客户端真实IP,多个IP按照','分割
-        if(ipAddress!=null && ipAddress.length()>15){ //"***.***.***.***".length() = 15
-            if(ipAddress.indexOf(",")>0){
-                ipAddress = ipAddress.substring(0,ipAddress.indexOf(","));
+        if (ipAddress != null && ipAddress.length() > 15) { //"***.***.***.***".length() = 15
+            if (ipAddress.indexOf(",") > 0) {
+                ipAddress = ipAddress.substring(0, ipAddress.indexOf(","));
             }
         }
 
@@ -62,17 +63,25 @@ public class IPConfig {
 
     public static String getAddressByIp(String ip) {
         try {
-            URL url = new URL("https://api.ip138.com/ip/?ip="+ip+"&datatype=jsonp&token=7d4a7771a18f25a3bebb982d63b924c0");
+            var stopWatch = new StopWatch("Get Country from IP:" + ip);
+            stopWatch.start("http request to IP138");
+            URL url = new URL("https://api.ip138.com/ip/?ip=" + ip + "&datatype=jsonp&token=7d4a7771a18f25a3bebb982d63b924c0");
             URLConnection conn = url.openConnection();
             BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream(), "utf-8"));
-            String line = null;
-            StringBuffer result = new StringBuffer();
+            String line;
+            StringBuilder result = new StringBuilder();
             while ((line = reader.readLine()) != null) {
                 result.append(line);
             }
             reader.close();
+            stopWatch.stop();
+            stopWatch.start("parse result");
             JSONObject jsStr = JSONObject.parseObject(result.toString());
             JSONArray address = JSONObject.parseArray(jsStr.get("data").toString());
+            stopWatch.stop();
+            if (stopWatch.getTotalTimeSeconds() > 2) {
+                log.warn(stopWatch.toString());
+            }
             return address.get(0).toString();
         } catch (IOException e) {
             return UNKNOWN;
