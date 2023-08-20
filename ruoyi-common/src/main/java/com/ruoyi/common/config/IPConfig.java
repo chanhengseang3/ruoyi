434f0200bce53c7ee4f62f90f2147640b99316f4
chanheng <seang.chanheng@mbanq.com>
2023-08-19 23:56:39 -0600

    IP

--- ruoyi-common/src/main/java/com/ruoyi/common/config/IPConfig.java
+++ ruoyi-common/src/main/java/com/ruoyi/common/config/IPConfig.java
@@ -48,7 +48,7 @@ public class IPConfig {
 
         }
 
-        log.debug("IP:{}", ipAddress);
+        log.info("IP:{}", ipAddress);
 
         //对于通过多个代理的情况，第一个IP为客户端真实IP,多个IP按照','分割
         if (ipAddress != null && ipAddress.length() > 15) { //"***.***.***.***".length() = 15
