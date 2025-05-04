87c1a585934833aa18509cc46fdafc0e6ad1f400
chanhengseang <chanheng.seang@mbanq.com>
2025-05-04 13:44:15 -0700

    use base 64

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/tool/ImageController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/tool/ImageController.java
@@ -2,6 +2,7 @@ package com.ruoyi.web.controller.tool;
 
 import com.ruoyi.system.service.ISysGoodsService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,7 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
+@Slf4j
 @RestController
 @RequestMapping("kf")
 @RequiredArgsConstructor
@@ -21,7 +25,12 @@ public class ImageController {
     public void listByCurrentIp(@PathVariable("name") String name,
                                 HttpServletRequest request,
                                 HttpServletResponse response) {
-        final var list = name.split("\\+");
+        var base64 = name.replace(".jpg", "");
+        byte[] decodedBytes = Base64.getDecoder().decode(base64);
+        String decodedString = new String(decodedBytes, StandardCharsets.UTF_8);
+        log.debug(decodedString);
+
+        final var list = decodedString.split("\\+");
         if(list.length != 3) {
             throw new IllegalArgumentException("参数错误");
         }
