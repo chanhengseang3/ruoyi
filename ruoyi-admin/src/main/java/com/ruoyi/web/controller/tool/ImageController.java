6f2411bcf4ca0dfa10b6a7b650285011738f794e
chanhengseang <chanheng.seang@mbanq.com>
2025-05-04 13:01:48 -0700

    add home

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/tool/ImageController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/tool/ImageController.java
@@ -17,12 +17,16 @@ public class ImageController {
 
     private final ISysGoodsService sysGoodsService;
 
-    @GetMapping("{goodsId}/{index}/{name}")
+    @GetMapping("{name}")
     public void listByCurrentIp(@PathVariable("name") String name,
-                                @PathVariable("goodsId") Long goodsId,
-                                @PathVariable("index") int index,
                                 HttpServletRequest request,
                                 HttpServletResponse response) {
+        final var list = name.split("\\+");
+        if(list.length != 3) {
+            throw new IllegalArgumentException("参数错误");
+        }
+        final var goodsId = Long.parseLong(list[0]);
+        final var index = Integer.parseInt(list[1]);
         sysGoodsService.listByCurrentIp(goodsId, index, request, response);
     }
 }
