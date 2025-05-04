05c10b0926475216bd2d8885a68b2b0b69f63dac
chanhengseang <chanheng.seang@mbanq.com>
2025-05-04 12:36:58 -0700

    change image path

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/tool/ImageController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/tool/ImageController.java
@@ -2,8 +2,6 @@ package com.ruoyi.web.controller.tool;
 
 import com.ruoyi.system.service.ISysGoodsService;
 import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,17 +15,14 @@ import javax.servlet.http.HttpServletResponse;
 @RequiredArgsConstructor
 public class ImageController {
 
-    private static final Logger LOG = LoggerFactory.getLogger(ImageController.class);
-
     private final ISysGoodsService sysGoodsService;
 
-    @GetMapping("{name}")
+    @GetMapping("{goodsId}/{index}/{name}")
     public void listByCurrentIp(@PathVariable("name") String name,
+                                @PathVariable("goodsId") Long goodsId,
+                                @PathVariable("index") int index,
                                 HttpServletRequest request,
                                 HttpServletResponse response) {
-
-        final long goodsId = 0L;
-        final int index = 0;
         sysGoodsService.listByCurrentIp(goodsId, index, request, response);
     }
 }
