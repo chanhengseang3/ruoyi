b996ec7e114117383418284fdce2c7d7a8e86337
chanhengseang <chanheng.seang@mbanq.com>
2025-05-06 12:16:46 -0700

    so done

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/tool/ImageController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/tool/ImageController.java
@@ -3,6 +3,10 @@ package com.ruoyi.web.controller.tool;
 import com.ruoyi.system.service.ISysGoodsService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.CacheControl;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.net.URI;
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 
@@ -21,32 +26,38 @@ public class ImageController {
 
     private final ISysGoodsService sysGoodsService;
 
-    @GetMapping("{name}")
-    public void listByCurrentIp(@PathVariable("name") String name,
-                                HttpServletRequest request,
-                                HttpServletResponse response) {
-        /*
+    @GetMapping("fw/{origin}")
+    public ResponseEntity<String> forward(@PathVariable String origin,
+                                          HttpServletRequest request) {
+
+        var base64 = origin.replace(".jpg", "");
+        byte[] decodedBytes = Base64.getDecoder().decode(base64);
+        String decodedString = new String(decodedBytes, StandardCharsets.UTF_8);
+        
+        // Get scheme and host directly from the request
+        String serverName = request.getServerName();
+
+        final var newUri = "https://%s/kf/%s.jpg".formatted(serverName, decodedString);
+
         HttpHeaders headers = new HttpHeaders();
         headers.setCacheControl(CacheControl.noCache());
         headers.setPragma("no-cache");
+        headers.setLocation(URI.create(newUri));
 
-        if (isWhiteIP) {
-            headers.setLocation(URI.create(white));
-        } else {
-            headers.setLocation(URI.create(black));
-        }
         return new ResponseEntity<>(headers, HttpStatus.FOUND); // 302
-         */
-        response.setHeader("Cache-Control", "no-cache");
+    }
+
+    @GetMapping("{name}")
+    public void listByCurrentIp(@PathVariable("name") String fileName,
+                                HttpServletRequest request,
+                                HttpServletResponse response) {
+
+        response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
         response.setHeader("Pragma", "no-cache");
+        var name = fileName.replace(".jpg", "");
 
-        var base64 = name.replace(".jpg", "");
-        byte[] decodedBytes = Base64.getDecoder().decode(base64);
-        String decodedString = new String(decodedBytes, StandardCharsets.UTF_8);
-        log.debug(decodedString);
-
-        final var list = decodedString.split("\\+");
-        if(list.length != 3) {
+        final var list = name.split("\\+");
+        if (list.length != 3) {
             throw new IllegalArgumentException("参数错误");
         }
         final var goodsId = Long.parseLong(list[0]);
